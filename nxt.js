e3544a2831f1802abee313b06971980df4faf76b
Anurag Uniyal <auniyal@vmware.com>
2016-01-06 23:47:55 -0800

    added facility to go forward or backwards and in the end double lick leads to next or prev page

--- nxt.js
+++ nxt.js
@@ -1,29 +1,71 @@
+javascript:
 /*
 Scrolls the next image into view, at the end optionally moves to next page
 Pass width and height to function which decides how small images to skip
+3rd param to function is dir 1=forward -1=backward
 */
 
-(function(w,h){
-var c=(document._c||-1)+1;
+(function(w,h,dir){
 var l = document.getElementsByTagName('img');
-for(var i=c;i<l.length;i++){
+var dir_txt = 'next';
+var c=document._au_c||-1;
+if(dir==-1){
+  dir_txt = 'prev';
+  c=document._au_c||l.length;
+}
+var i=c;
+while(1){
+  i += dir;
+  if(dir==1 && i == l.length){
+    break;
+  }
+  if(dir==-1 && i == -1){
+    break;
+  }
   var e = l[i];
   if(e.width < w) continue;
   if(e.height < h) continue;
-  document._c = c = i;
+  document._au_c = c = i;
+  // if we are showing anothere image reset done state
+  document._au_done=false;
   e.scrollIntoView();
-  e.style.height = window. innerHeight +'px';
+  e.style.height = window.innerHeight +'px';
   return;
 }
-if(confirm('Done with all images, go to next page?')){
+
+function change_page(){
   var l = document.getElementsByTagName('a');
   for(var i=0;i<l.length;i++){
     var e = l[i];
-    if(e.rel=='next'||e.title.match(/next/i)){
+    if(dir == 1 &&(e.rel=='next'||e.title.match(/next/i))){
+      e.click();
+      return;
+    }
+    if(dir == -1 &&(e.rel=='prev'||e.title.match(/prev/i))){
       e.click();
       return;
     }
   }
-  alert("No next link found.");
+  alert("No "+dir_txt+" link found.");
 }
-})(100, 300);
+function show_msg(msg, duration){
+  var el = document.createElement("div");
+  el.setAttribute("style","position:absolute;top:40%;left:20%;padding:10px;background-color:white;color:red;border:2px solid red;");
+  el.innerHTML = msg;
+  setTimeout(function(){
+    el.parentNode.removeChild(el);
+  },duration*1000);
+  document.body.appendChild(el);
+  el.scrollIntoView();
+  console.log(el)
+}
+/* if we are done go to next page */
+if(document._au_done){
+  change_page();
+  return
+}
+/* we are done show msg to user*/
+show_msg("Reached the end of the page, to go to "+dir_txt+" page, click "+dir_txt+" again <br><small>msg will close in 10 secs</small>", 10);
+document._au_done=true;
+})(100, 300, -1);
+
